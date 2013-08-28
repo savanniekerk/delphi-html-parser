21e9952e26e24eb09dd26a4335c55cb68cb04bab
wr960204@126.com <wr960204@126.com@e6ae8d0e-91b2-b39d-ab6d-4526320c4856>
2013-08-28 07:04:06 +0000

    add support css selector tagname = "*"

--- trunk/trunk/demo/Unit4.pas
+++ trunk/trunk/demo/Unit4.pas
@@ -7,13 +7,11 @@ uses
   Windows, Messages, SysUtils, Variants,
   Classes, Graphics,
   Controls, Forms, Dialogs, StdCtrls, ComCtrls, IdBaseComponent,
-  IdComponent, IdTCPConnection, IdTCPClient, IdHTTP, ExtCtrls, xmldom,
-  XMLIntf, msxmldom, XMLDoc;
+  IdComponent, IdTCPConnection, IdTCPClient, IdHTTP, ExtCtrls;
 
 type
   TForm4 = class(TForm)
     Button1: TButton;
-    TreeView1: TTreeView;
     Memo2: TMemo;
     IdHTTP1: TIdHTTP;
     Panel1: TPanel;
@@ -22,12 +20,22 @@ type
     Button2: TButton;
     Button3: TButton;
     OpenDialog1: TOpenDialog;
-    XMLDocument1: TXMLDocument;
+    Panel2: TPanel;
+    GroupBox1: TGroupBox;
+    TreeView1: TTreeView;
+    GroupBox2: TGroupBox;
+    ListBox1: TListBox;
+    Panel3: TPanel;
+    Label1: TLabel;
+    Edit2: TEdit;
+    Button4: TButton;
     procedure Button1Click(Sender: TObject);
     procedure TreeView1Change(Sender: TObject; Node: TTreeNode);
     procedure Button2Click(Sender: TObject);
     procedure Button3Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
   private
     { Private declarations }
     FNodes: IHtmlElement;
@@ -74,18 +82,17 @@ begin
 end;
 
 procedure TForm4.Button1Click(Sender: TObject);
-var
-  l : IHtmlElementList;
+
 begin
   FNodes := parserHtml(Memo1.Text);
-  l := FNodes.SimpleCSSSelector('div#header ~ div#main div#container [role]');
-  if l.Count > 0 then
-     ShowMessage(Format('I fond the node "%s".',[l[0].Orignal]));
   TreeView1.Items.BeginUpdate;
   TreeView1.Items.Clear;
   NodesToTree1(TreeView1, FNodes, nil, 0);
   TreeView1.Items.EndUpdate;
   Caption := 'OK';
+  //
+  GroupBox2.Enabled := True;
+  ListBox1.Items.Clear;
 end;
 
 function EnumAttr(AParam: Pointer; const AttributeName, AttributeValue
@@ -108,7 +115,7 @@ begin
     try
       Memo2.Lines.BeginUpdate;
       Memo2.Lines.Clear;
-       //{
+      // {
       Memo2.Lines.Add(Format('ÐÐºÅ:%d,%d', [e.GetSourceLineNum,
         e.GetSourceColNum]));
       Memo2.Lines.Add('==================Orignal===========================');
@@ -126,8 +133,8 @@ begin
       Memo2.Lines.Add('==================InnerHTML===========================');
 
       Memo2.Lines.Add(e.InnerHtml);
-       //}
-      //Memo2.Text := e.InnerText;
+      // }
+      // Memo2.Text := e.InnerText;
       Memo2.Lines.EndUpdate;
     finally
       Pointer(e) := nil;
@@ -146,10 +153,10 @@ begin
     url := 'http://' + url;
   // Memo1.Lines.text := UTF8Decode(IdHTTP1.Get(url));
   s := IdHTTP1.Get(url);
-  {$IFDEF UNICODE}
+{$IFDEF UNICODE}
   Memo1.Lines.Text := s;
-  {$ELSE}
-   Memo1.Lines.text := UTF8Decode(IdHTTP1.Get(url));
+{$ELSE}
+  Memo1.Lines.Text := UTF8Decode(IdHTTP1.Get(url));
 {$ENDIF}
 end;
 
@@ -162,14 +169,51 @@ begin
   end;
 end;
 
+procedure TForm4.Button4Click(Sender: TObject);
+var
+  s : string;
+  l: IHtmlElementList;
+  i: Integer;
+begin
+  if FNodes <> nil then
+  begin
+    try
+      s := Edit2.Text;
+      l := FNodes.SimpleCSSSelector(s);
+    except
+      l := nil;
+    end;
+    if l <> nil then
+    begin
+      ListBox1.Items.BeginUpdate;
+      try
+        ListBox1.Items.Clear;
+        for i := 0 to l.Count - 1 do
+        begin
+          ListBox1.Items.Add(l[i].Orignal);
+        end;
+      finally
+        ListBox1.Items.EndUpdate;
+      end;
+    end;
+  end;
+end;
+
+procedure TForm4.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+  FNodes := nil;
+end;
+
 procedure TForm4.FormCreate(Sender: TObject);
 begin
-  //XMLDocument1.DocumentElement.NextSibling
+  GroupBox2.Enabled := False;
 end;
 
 initialization
 
-// ReportMemoryLeaksOnShutdown := True;
+{$IF Declared(ReportMemoryLeaksOnShutdown)}
+  ReportMemoryLeaksOnShutdown := True;
+{$IFEND}
 
 finalization
 
